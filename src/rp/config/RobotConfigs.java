a5d7658d4094cddf2161aa428caa2d9e0133ad7f
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2015-01-15 11:31:04 +0000

    Added project files to git ignore, and updated Express Bot config for new build.

--- src/rp/config/RobotConfigs.java
+++ src/rp/config/RobotConfigs.java
@@ -25,7 +25,7 @@ public class RobotConfigs {
 	 * Configuration for Nick's Expressbot build. The numbers may be different for you. Measurements in mm. 
 	 */
 	public static final WheeledRobotConfiguration EXPRESS_BOT = new WheeledRobotConfiguration(
-			56, 163, 240, Motor.C, Motor.A);
+			56, 163, 210, Motor.A, Motor.C);
 
 	/**
 	 * Configuration for Nick's Castorbot build. The numbers may be different for you. Measurements in mm. 
